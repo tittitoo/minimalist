e1d46319c9eef1b2bc52f50f64232f7a3e53c2b8
Thiha Aung <infowizard@gmail.com>
2023-03-22 02:05:46 +0800

    Implemented summary function

--- functions.py
+++ functions.py
@@ -52,7 +52,7 @@ def set_x(text):
     return text
 
 # Take in sheet and workbook
-def fill_formula(wb, sheet):
+def fill_formula(sheet):
     # Formula to cells
     last_row = sheet.range('C100000').end('up').row
     sheet.range('A1').formula = '= "JASON REF: " & Config!B29 &  ", REVISION: " &  Config!B30 & ", PROJECT: " & Config!B26'
@@ -63,11 +63,8 @@ def fill_formula(wb, sheet):
     # Exchange rates
     sheet.range('Q3:Q' + str(last_row)).formula = '=IF(Config!B12="SGD",IF(J3<>"",VLOOKUP(J3,Config!$A$2:$B$10,2,FALSE),""),IF(J3<>"",VLOOKUP(J3,Config!$A$2:$B$10,2,FALSE)/VLOOKUP(Config!$B$12,Config!$A$2:$B$10,2,FALSE),""))'
     sheet.range('R3:R' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"") ,N3*Q3,"")'
-    # sheet.range('S3:S' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION") ,D3*R3,"")'
     sheet.range('S3:S' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION") ,D3*R3,"")'
     sheet.range('T3:T' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""), (R3*(1+$L$1+$N$1+$P$1+$R$1))/(1-0.05),"")'
-    # Below formula does not capture all options yet
-    # sheet.range('U3:U' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),IF(H3="OPTION","",D3*T3),"")'
     sheet.range('U3:U' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION",INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))<>"OPTION"),D3*T3,"")'
     sheet.range('V3:V' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$L$1,"")'
     sheet.range('W3:W' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$N$1,"")'
@@ -100,27 +97,35 @@ def fill_formula(wb, sheet):
     # sheet.range('AJ3:AJ' + str(last_row)).formula = '=IF(AND(AL3="Title", D3=1, E3="lot"), SUM(INDIRECT(CONCAT("AF", ROW()+1, ":AF",((MATCH("Title",INDIRECT(CONCAT("AL", ROW()+1, ":AL", MATCH(REPT("z",50),AL:AL))),0)) + ROW())))), "")'
     sheet.range('AJ3:AJ' + str(last_row)).formula = '=IF(AND(AL3="Title", D3=1, E3="lot"), SUM(INDIRECT(CONCAT("AI", ROW()+1, ":AI",((MATCH("Title",INDIRECT(CONCAT("AL", ROW()+1, ":AL", MATCH(REPT("z",50),AL:AL))),0)) + ROW())))), "")'
     sheet.range('AK3:AK' + str(last_row)).formula = '=IF(AL3="Lineitem", IF(ISNUMBER(INDIRECT(CONCAT("AJ",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))), "Lumpsum", "Unit Price"), "")'
-    (wb.sheets['Config'].range('100:100')).copy(sheet.range(str(last_row+2) + ':' + str(last_row+2)))
-    sheet.range('F'+ str(last_row+2)).formula = '="Subtotal(" & Config!B12 & ")"'
-    sheet.range('F'+ str(last_row+2)).font.bold = True
-    sheet.range('F'+ str(last_row+2)).font.size = 9
-    sheet.range('G' + str(last_row+2)).formula = '=SUM(G3:G' + str(last_row+1) + ')'
-    sheet.range('G' + str(last_row+2)).font.bold = True
-    sheet.range('U' + str(last_row+2)).formula = '=SUM(U3:U' + str(last_row+1) + ')'
-    sheet.range('U' + str(last_row+2)).font.bold = True
-    sheet.range('AF' + str(last_row+2)).formula = '=SUM(AF3:AF' + str(last_row+1) + ')'
-    # sheet.range('AF' + str(last_row+2)).formula = '=SUMIF(H3:H' + str(last_row+1) + ',"<>OPTION",AF3:AF' + str(last_row+1) + ')'
-    sheet.range('AF' + str(last_row+2)).font.bold = True
-    sheet.range('AG' + str(last_row+2)).formula = '=SUM(AG3:AG' + str(last_row+1) + ')'
-    sheet.range('AG' + str(last_row+2)).font.bold = True
-    sheet.range('AH' + str(last_row+2)).formula = '=AG' + str(last_row+2) + '/AF' + str(last_row+2)
-    sheet.range('AH' + str(last_row+2)).font.bold = True
 
-    # Set-up print area
-    sheet.page_setup.print_area = 'A1:H' + str(last_row+2)
+def fill_lastrow (wb):
+    skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
+
+    for sheet in wb.sheet_names:
+       if sheet not in skip_sheets:
+            sheet = wb.sheets[sheet]
+            last_row = sheet.range('G100000').end('up').row
+            (wb.sheets['Config'].range('100:100')).copy(sheet.range(str(last_row+2) + ':' + str(last_row+2)))
+            sheet.range('F'+ str(last_row+2)).formula = '="Subtotal(" & Config!B12 & ")"'
+            sheet.range('F'+ str(last_row+2)).font.bold = True
+            sheet.range('F'+ str(last_row+2)).font.size = 9
+            sheet.range('G' + str(last_row+2)).formula = '=SUM(G3:G' + str(last_row+1) + ')'
+            sheet.range('G' + str(last_row+2)).font.bold = True
+            sheet.range('U' + str(last_row+2)).formula = '=SUM(U3:U' + str(last_row+1) + ')'
+            sheet.range('U' + str(last_row+2)).font.bold = True
+            sheet.range('AF' + str(last_row+2)).formula = '=SUM(AF3:AF' + str(last_row+1) + ')'
+            # sheet.range('AF' + str(last_row+2)).formula = '=SUMIF(H3:H' + str(last_row+1) + ',"<>OPTION",AF3:AF' + str(last_row+1) + ')'
+            sheet.range('AF' + str(last_row+2)).font.bold = True
+            sheet.range('AG' + str(last_row+2)).formula = '=SUM(AG3:AG' + str(last_row+1) + ')'
+            sheet.range('AG' + str(last_row+2)).font.bold = True
+            sheet.range('AH' + str(last_row+2)).formula = '=AG' + str(last_row+2) + '/AF' + str(last_row+2)
+            sheet.range('AH' + str(last_row+2)).font.bold = True
+
+            # Set-up print area
+            sheet.page_setup.print_area = 'A1:H' + str(last_row+2)
 
     # For formatting
-def format(wb, sheet):
+def format(sheet):
     sheet.range('B:B').autofit()
     sheet.range('C:C').autofit()
     sheet.range('C:C').column_width = 55
@@ -159,3 +164,61 @@ def format(wb, sheet):
     sheet.range('AL:AL').autofit()
     sheet.range('AM:AM').autofit()
     sheet.range('AN:AN').autofit()
+
+def summary(wb, discount=False):
+    summary_formula = []
+    collect = [] # Collect formula to be put in summary page.
+    formula_fragment = '=IF(OR(Config!B13="COMMERCIAL PROPOSAL", Config!B13="BUDGETARY PROPOSAL"),'
+    skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
+
+    for sheet in wb.sheet_names:
+       if sheet not in skip_sheets:
+            sheet = wb.sheets[sheet]
+            last_row = sheet.range('G100000').end('up').row
+            collect = [ formula_fragment + "'" + sheet.name + "'!$G$" + str(last_row) + ', "")',
+                        formula_fragment + "'" + sheet.name + "'!$U$" + str(last_row) + ', "")']
+                #    "='" + sheet.name + "'!$AF$" + str(last_row)]
+            summary_formula.extend(collect)
+            collect = []
+
+    count = 1
+    offset = 20
+    odered_summary_formula = summary_formula[::-1]
+    sheet = wb.sheets['Summary']
+    for system in wb.sheet_names:
+        if system not in skip_sheets:
+            sheet.range('B' + str(offset)).value = count
+            sheet.range('C' + str(offset)).value = system
+            sheet.range('D' + str(offset)).formula = odered_summary_formula.pop()
+            sheet.range('H' + str(offset)).formula = odered_summary_formula.pop()
+            sheet.range('I' + str(offset)).formula = '=IF(H'+ str(offset) + '<>"",D' + str(offset) + '- H' + str(offset) + ',"")'
+            sheet.range('J' + str(offset)).formula = '=IF(I' + str(offset) + '<>"",I' + str(offset) + '/D' + str(offset) + ',"")'
+            count += 1
+            offset += 1
+    
+    # Drawing lines
+    (wb.sheets['Config'].range('106:106')).copy(sheet.range(str(offset) + ':' + str(offset)))
+    (wb.sheets['Config'].range('102:102')).copy(sheet.range(str(offset+1) + ':' + str(offset+1)))
+    # sheet = wb.sheets['Summary']
+    sheet.range('C' + str(offset+1)).value = '="TOTAL PROJECT (" & Config!B12 & ")"'
+    sheet.range('D' + str(offset+1)).formula = '=SUMIF(E20:E' + str(offset) + ',"<>OPTION",D20:D' + str(offset) + ')'
+    sheet.range('E' + str(offset+1)).formula = '=IF(COUNTIF(E20:E' + str(offset) + ',"OPTION"), "Excluding Option", "")'
+    sheet.range('H' + str(offset+1)).formula = '=SUMIF(E20:E' + str(offset) + ',"<>OPTION",H20:H' + str(offset) + ')'
+    sheet.range('I' + str(offset+1)).formula = '=IF(H'+ str(offset+1) + '<>"", D' + str(offset+1) + '- H' + str(offset+1) + ',"")'
+    sheet.range('J' + str(offset+1)).formula = '=IF(I' + str(offset+1) + '<>0,I' + str(offset+1) + '/D' + str(offset+1) + ',"")'
+    if discount:
+        (wb.sheets['Config'].range('103:103')).copy(sheet.range(str(offset+2) + ':' + str(offset+2)))
+        (wb.sheets['Config'].range('104:104')).copy(sheet.range(str(offset+3) + ':' + str(offset+3)))
+        sheet.range('C' + str(offset+3)).formula = '="TOTAL PROJECT PRICE AFTER DISCOUNT (" & Config!B12 & ")"'
+        sheet.range('D' + str(offset+3)).formula = '=SUM(D' +str(offset+1) + ':D' + str(offset+2) + ')'
+        sheet.range('H' + str(offset+3)).formula = '=$H$' +str(offset+1)
+        sheet.range('I' + str(offset+3)).formula = '=IF(H'+ str(offset+3) + '<>"", D' + str(offset+3) + '- H' + str(offset+3) + ',"")'
+        sheet.range('J' + str(offset+3)).formula = '=IF(I' + str(offset+3) + '<>0,I' + str(offset+3) + '/D' + str(offset+3) + ',"")'
+        sheet.range('C' + str(offset+5)).formula = '="• All the prices are in " & Config!B12 & " excluding GST."'
+        sheet.range('C' + str(offset+6)).value = "• Total project price does not include prices for optional items set out in the detailed bill of material."
+    else:
+        sheet.range('C' + str(offset+3)).formula = '="• All the prices are in " & Config!B12 & " excluding GST."'
+        sheet.range('C' + str(offset+4)).value = "• Total project price does not include prices for optional items set out in the detailed bill of material."
+
+    last_row = sheet.range('C100000').end('up').row
+    sheet.page_setup.print_area = 'A1:F' + str(last_row+3)
