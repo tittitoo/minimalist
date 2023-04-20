b08cf3a7bf51fd3fe6dc6362dc006bb68f92190e
Thiha Aung <infowizard@gmail.com>
2023-04-20 10:19:12 +0800

    Add guards in functions.py

--- functions.py
+++ functions.py
@@ -62,50 +62,52 @@ def set_x(text):
 
 # Take in sheet and workbook
 def fill_formula(sheet):
-    # Formula to cells
-    last_row = sheet.range('C100000').end('up').row
-    sheet.range('A1').formula = '= "JASON REF: " & Config!B29 &  ", REVISION: " &  Config!B30 & ", PROJECT: " & Config!B26'
-    # Serail Numbering (SN)
-    sheet.range('B3:B' + str(last_row)).formula = '=IF(AND(ISNUMBER(D3), ISNUMBER(K3), XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)), COUNT(INDIRECT(CONCAT("B",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1),":B",ROW()-1))) + 1, "")'
-    sheet.range('N3:N' + str(last_row)).formula = '=IF(K3<>"",K3*(1-M3),"")'
-    sheet.range('O3:O' + str(last_row)).formula = '=IF(AND(D3<>"", K3<>"",H3<>"OPTION"),D3*N3,"")'
-    # Exchange rates
-    sheet.range('Q3:Q' + str(last_row)).formula = '=IF(Config!B12="SGD",IF(J3<>"",VLOOKUP(J3,Config!$A$2:$B$10,2,FALSE),""),IF(J3<>"",VLOOKUP(J3,Config!$A$2:$B$10,2,FALSE)/VLOOKUP(Config!$B$12,Config!$A$2:$B$10,2,FALSE),""))'
-    sheet.range('R3:R' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"") ,N3*Q3,"")'
-    sheet.range('S3:S' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION") ,D3*R3,"")'
-    sheet.range('T3:T' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""), (R3*(1+$L$1+$N$1+$P$1+$R$1))/(1-0.05),"")'
-    sheet.range('U3:U' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION",INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))<>"OPTION"),D3*T3,"")'
-    sheet.range('V3:V' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$L$1,"")'
-    sheet.range('W3:W' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$N$1,"")'
-    sheet.range('X3:X' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$P$1,"")'
-    sheet.range('Y3:Y' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$R$1,"")'
-    sheet.range('Z3:Z' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),T3-(R3+V3+W3+X3+Y3),"")'
-    sheet.range('AA3:AA' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),$J$1,"")'
-    sheet.range('AC3:AC' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),CEILING(T3/(1-AA3), 1),"")'
-    # sheet.range('AD3:AD' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION",H3<>"INCLUDED"),D3*AC3,"")'
-    sheet.range('AD3:AD' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION",H3<>"INCLUDED",(INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))) <>"OPTION"),D3*AC3,"")'
-    sheet.range('AE3:AE' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),IF(AB3<>"",AB3,AC3),"")'
-    # sheet.range('AF3:AF' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION", H3<>"INCLUDED"),D3*AE3,"")'
-    sheet.range('AF3:AF' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION", H3<>"INCLUDED",(INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))) <>"OPTION"),D3*AE3,"")'
-    # sheet.range('AF3:AF' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),D3*AE3,"")'
-    sheet.range('AG3:AG' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION", H3<>"INCLUDED",AF3<>""),AF3-U3,"")'
-    sheet.range('AH3:AH' + str(last_row)).formula = '=IF(AND(AG3<>"",AG3<>0),AG3/AF3,"")'
-    sheet.range('AI3:AI' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION"),D3*AE3,"")'
-    # sheet.range('AL3:AL' + str(last_row)).formula = '=IF(A3<>"","Title",IF(B3<>"","Lineitem",IF(LEFT(C3,3)="***","Comment",IF(AND(A3="",B3="",C2="", C4<>"",D4<>""), "Subtitle",""))))'
-    # Unit Price
-    sheet.range('F3:F' + str(last_row)).formula = '=IF(AND(AL3="Title", ISNUMBER(AJ3)), AJ3, IF(AND(AL3="Lineitem", AK3="Lumpsum", H3<>"OPTION"), "", AE3))'
-    # sheet.range('F3:F' + str(last_row)).formula = '=IF(AE3<>"", AE3,"")'
-    sheet.range('G3:G' + str(last_row)).formula = '=IF(AND(F3<>"", H3<>"OPTION", H3<>"INCLUDED"), D3*F3,"")'
-    sheet.range('L3:L' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION"),D3*K3,"")'
-    # For Format field
-    sheet.range('AL1').value = "Title"
-    sheet.range('AL3:AL' + str(last_row)).formula = '=IF(A3<>"","Title", IF(B3<>"","Lineitem", IF(LEFT(C3,3)="***","Comment", IF(AND(A3="",B3="",C2="", C4<>"",D4<>""), "Subtitle",""))))'
-    sheet.range('AL' + str(last_row+1)).value = "Title"
+    skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
+    if sheet not in skip_sheets:
+        # Formula to cells
+        last_row = sheet.range('C100000').end('up').row
+        sheet.range('A1').formula = '= "JASON REF: " & Config!B29 &  ", REVISION: " &  Config!B30 & ", PROJECT: " & Config!B26'
+        # Serail Numbering (SN)
+        sheet.range('B3:B' + str(last_row)).formula = '=IF(AND(ISNUMBER(D3), ISNUMBER(K3), XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)), COUNT(INDIRECT(CONCAT("B",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1),":B",ROW()-1))) + 1, "")'
+        sheet.range('N3:N' + str(last_row)).formula = '=IF(K3<>"",K3*(1-M3),"")'
+        sheet.range('O3:O' + str(last_row)).formula = '=IF(AND(D3<>"", K3<>"",H3<>"OPTION"),D3*N3,"")'
+        # Exchange rates
+        sheet.range('Q3:Q' + str(last_row)).formula = '=IF(Config!B12="SGD",IF(J3<>"",VLOOKUP(J3,Config!$A$2:$B$10,2,FALSE),""),IF(J3<>"",VLOOKUP(J3,Config!$A$2:$B$10,2,FALSE)/VLOOKUP(Config!$B$12,Config!$A$2:$B$10,2,FALSE),""))'
+        sheet.range('R3:R' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"") ,N3*Q3,"")'
+        sheet.range('S3:S' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION") ,D3*R3,"")'
+        sheet.range('T3:T' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""), (R3*(1+$L$1+$N$1+$P$1+$R$1))/(1-0.05),"")'
+        sheet.range('U3:U' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION",INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))<>"OPTION"),D3*T3,"")'
+        sheet.range('V3:V' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$L$1,"")'
+        sheet.range('W3:W' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$N$1,"")'
+        sheet.range('X3:X' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$P$1,"")'
+        sheet.range('Y3:Y' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),R3*$R$1,"")'
+        sheet.range('Z3:Z' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),T3-(R3+V3+W3+X3+Y3),"")'
+        sheet.range('AA3:AA' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),$J$1,"")'
+        sheet.range('AC3:AC' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),CEILING(T3/(1-AA3), 1),"")'
+        # sheet.range('AD3:AD' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION",H3<>"INCLUDED"),D3*AC3,"")'
+        sheet.range('AD3:AD' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION",H3<>"INCLUDED",(INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))) <>"OPTION"),D3*AC3,"")'
+        sheet.range('AE3:AE' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),IF(AB3<>"",AB3,AC3),"")'
+        # sheet.range('AF3:AF' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION", H3<>"INCLUDED"),D3*AE3,"")'
+        sheet.range('AF3:AF' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION", H3<>"INCLUDED",(INDIRECT(CONCAT("H",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))) <>"OPTION"),D3*AE3,"")'
+        # sheet.range('AF3:AF' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>""),D3*AE3,"")'
+        sheet.range('AG3:AG' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION", H3<>"INCLUDED",AF3<>""),AF3-U3,"")'
+        sheet.range('AH3:AH' + str(last_row)).formula = '=IF(AND(AG3<>"",AG3<>0),AG3/AF3,"")'
+        sheet.range('AI3:AI' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"", H3<>"OPTION"),D3*AE3,"")'
+        # sheet.range('AL3:AL' + str(last_row)).formula = '=IF(A3<>"","Title",IF(B3<>"","Lineitem",IF(LEFT(C3,3)="***","Comment",IF(AND(A3="",B3="",C2="", C4<>"",D4<>""), "Subtitle",""))))'
+        # Unit Price
+        sheet.range('F3:F' + str(last_row)).formula = '=IF(AND(AL3="Title", ISNUMBER(AJ3)), AJ3, IF(AND(AL3="Lineitem", AK3="Lumpsum", H3<>"OPTION"), "", AE3))'
+        # sheet.range('F3:F' + str(last_row)).formula = '=IF(AE3<>"", AE3,"")'
+        sheet.range('G3:G' + str(last_row)).formula = '=IF(AND(F3<>"", H3<>"OPTION", H3<>"INCLUDED"), D3*F3,"")'
+        sheet.range('L3:L' + str(last_row)).formula = '=IF(AND(D3<>"",K3<>"",H3<>"OPTION"),D3*K3,"")'
+        # For Format field
+        sheet.range('AL1').value = "Title"
+        sheet.range('AL3:AL' + str(last_row)).formula = '=IF(A3<>"","Title", IF(B3<>"","Lineitem", IF(LEFT(C3,3)="***","Comment", IF(AND(A3="",B3="",C2="", C4<>"",D4<>""), "Subtitle",""))))'
+        sheet.range('AL' + str(last_row+1)).value = "Title"
 
-    # For Lumpsum
-    # sheet.range('AJ3:AJ' + str(last_row)).formula = '=IF(AND(AL3="Title", D3=1, E3="lot"), SUM(INDIRECT(CONCAT("AF", ROW()+1, ":AF",((MATCH("Title",INDIRECT(CONCAT("AL", ROW()+1, ":AL", MATCH(REPT("z",50),AL:AL))),0)) + ROW())))), "")'
-    sheet.range('AJ3:AJ' + str(last_row)).formula = '=IF(AND(AL3="Title", D3=1, E3="lot"), SUM(INDIRECT(CONCAT("AI", ROW()+1, ":AI",((MATCH("Title",INDIRECT(CONCAT("AL", ROW()+1, ":AL", MATCH(REPT("z",50),AL:AL))),0)) + ROW())))), "")'
-    sheet.range('AK3:AK' + str(last_row)).formula = '=IF(AL3="Lineitem", IF(ISNUMBER(INDIRECT(CONCAT("AJ",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))), "Lumpsum", "Unit Price"), "")'
+        # For Lumpsum
+        # sheet.range('AJ3:AJ' + str(last_row)).formula = '=IF(AND(AL3="Title", D3=1, E3="lot"), SUM(INDIRECT(CONCAT("AF", ROW()+1, ":AF",((MATCH("Title",INDIRECT(CONCAT("AL", ROW()+1, ":AL", MATCH(REPT("z",50),AL:AL))),0)) + ROW())))), "")'
+        sheet.range('AJ3:AJ' + str(last_row)).formula = '=IF(AND(AL3="Title", D3=1, E3="lot"), SUM(INDIRECT(CONCAT("AI", ROW()+1, ":AI",((MATCH("Title",INDIRECT(CONCAT("AL", ROW()+1, ":AL", MATCH(REPT("z",50),AL:AL))),0)) + ROW())))), "")'
+        sheet.range('AK3:AK' + str(last_row)).formula = '=IF(AL3="Lineitem", IF(ISNUMBER(INDIRECT(CONCAT("AJ",XMATCH("Title",(INDIRECT(CONCAT("AL1:","AL",ROW()-1))),0,-1)))), "Lumpsum", "Unit Price"), "")'
 
 def fill_lastrow (wb):
     skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
@@ -134,81 +136,87 @@ def fill_lastrow (wb):
             sheet.page_setup.print_area = 'A1:H' + str(last_row+2)
 
 def fill_lastrow_sheet(wb, sheet):
-    last_row = sheet.range('C100000').end('up').row
-    (wb.sheets['Config'].range('100:100')).copy(sheet.range(str(last_row+2) + ':' + str(last_row+2)))
-    sheet.range('F'+ str(last_row+2)).formula = '="Subtotal(" & Config!B12 & ")"'
-    sheet.range('F'+ str(last_row+2)).font.bold = True
-    sheet.range('F'+ str(last_row+2)).font.size = 9
-    sheet.range('G' + str(last_row+2)).formula = '=SUM(G3:G' + str(last_row+1) + ')'
-    sheet.range('G' + str(last_row+2)).font.bold = True
-    sheet.range('U' + str(last_row+2)).formula = '=SUM(U3:U' + str(last_row+1) + ')'
-    sheet.range('U' + str(last_row+2)).font.bold = True
-    sheet.range('AF' + str(last_row+2)).formula = '=SUM(AF3:AF' + str(last_row+1) + ')'
-    # sheet.range('AF' + str(last_row+2)).formula = '=SUMIF(H3:H' + str(last_row+1) + ',"<>OPTION",AF3:AF' + str(last_row+1) + ')'
-    sheet.range('AF' + str(last_row+2)).font.bold = True
-    sheet.range('AG' + str(last_row+2)).formula = '=SUM(AG3:AG' + str(last_row+1) + ')'
-    sheet.range('AG' + str(last_row+2)).font.bold = True
-    sheet.range('AH' + str(last_row+2)).formula = '=AG' + str(last_row+2) + '/AF' + str(last_row+2)
-    sheet.range('AH' + str(last_row+2)).font.bold = True
+    skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
+    if sheet not in skip_sheets:
+        last_row = sheet.range('C100000').end('up').row
+        (wb.sheets['Config'].range('100:100')).copy(sheet.range(str(last_row+2) + ':' + str(last_row+2)))
+        sheet.range('F'+ str(last_row+2)).formula = '="Subtotal(" & Config!B12 & ")"'
+        sheet.range('F'+ str(last_row+2)).font.bold = True
+        sheet.range('F'+ str(last_row+2)).font.size = 9
+        sheet.range('G' + str(last_row+2)).formula = '=SUM(G3:G' + str(last_row+1) + ')'
+        sheet.range('G' + str(last_row+2)).font.bold = True
+        sheet.range('U' + str(last_row+2)).formula = '=SUM(U3:U' + str(last_row+1) + ')'
+        sheet.range('U' + str(last_row+2)).font.bold = True
+        sheet.range('AF' + str(last_row+2)).formula = '=SUM(AF3:AF' + str(last_row+1) + ')'
+        # sheet.range('AF' + str(last_row+2)).formula = '=SUMIF(H3:H' + str(last_row+1) + ',"<>OPTION",AF3:AF' + str(last_row+1) + ')'
+        sheet.range('AF' + str(last_row+2)).font.bold = True
+        sheet.range('AG' + str(last_row+2)).formula = '=SUM(AG3:AG' + str(last_row+1) + ')'
+        sheet.range('AG' + str(last_row+2)).font.bold = True
+        sheet.range('AH' + str(last_row+2)).formula = '=AG' + str(last_row+2) + '/AF' + str(last_row+2)
+        sheet.range('AH' + str(last_row+2)).font.bold = True
 
-    # Set-up print area
-    sheet.page_setup.print_area = 'A1:H' + str(last_row+2)
+        # Set-up print area
+        sheet.page_setup.print_area = 'A1:H' + str(last_row+2)
 
 
     # For formatting
 def format(sheet):
-    sheet.range('A:A').column_width = 3
-    sheet.range('B:B').autofit()
-    sheet.range('C:C').autofit()
-    sheet.range('C:C').column_width = 55
-    sheet.range('C:C').wrap_text = True
-    sheet.range('D:D').autofit()
-    sheet.range('E:E').autofit()
-    sheet.range('F:F').autofit()
-    sheet.range('G:G').autofit()
-    sheet.range('H:H').autofit()
-    sheet.range('J:J').autofit()
-    sheet.range('K:K').autofit()
-    sheet.range('L:L').autofit()
-    sheet.range('M:M').autofit()
-    sheet.range('N:N').autofit()
-    sheet.range('O:O').autofit()
-    sheet.range('Q:Q').autofit()
-    sheet.range('R:R').autofit()
-    sheet.range('S:S').autofit()
-    sheet.range('T:T').autofit()
-    sheet.range('U:U').autofit()
-    sheet.range('V:V').autofit()
-    sheet.range('W:W').autofit()
-    sheet.range('X:X').autofit()
-    sheet.range('Y:Y').autofit()
-    sheet.range('Z:Z').autofit()
-    sheet.range('AA:AA').autofit()
-    sheet.range('AB:AB').autofit()
-    sheet.range('AC:AC').autofit()
-    sheet.range('AD:AD').autofit()
-    sheet.range('AE:AE').autofit()
-    sheet.range('AF:AF').autofit()
-    sheet.range('AG:AG').autofit()
-    sheet.range('AH:AH').autofit()
-    sheet.range('AI:AI').autofit()
-    sheet.range('AJ:AJ').autofit()
-    sheet.range('AK:AK').autofit()
-    sheet.range('AL:AL').autofit()
-    sheet.range('AM:AM').autofit()
-    sheet.range('AN:AN').autofit()
+    skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
+    if sheet not in skip_sheets:
+        sheet.range('A:A').column_width = 3
+        sheet.range('B:B').autofit()
+        sheet.range('C:C').autofit()
+        sheet.range('C:C').column_width = 55
+        sheet.range('C:C').wrap_text = True
+        sheet.range('D:D').autofit()
+        sheet.range('E:E').autofit()
+        sheet.range('F:F').autofit()
+        sheet.range('G:G').autofit()
+        sheet.range('H:H').autofit()
+        sheet.range('J:J').autofit()
+        sheet.range('K:K').autofit()
+        sheet.range('L:L').autofit()
+        sheet.range('M:M').autofit()
+        sheet.range('N:N').autofit()
+        sheet.range('O:O').autofit()
+        sheet.range('Q:Q').autofit()
+        sheet.range('R:R').autofit()
+        sheet.range('S:S').autofit()
+        sheet.range('T:T').autofit()
+        sheet.range('U:U').autofit()
+        sheet.range('V:V').autofit()
+        sheet.range('W:W').autofit()
+        sheet.range('X:X').autofit()
+        sheet.range('Y:Y').autofit()
+        sheet.range('Z:Z').autofit()
+        sheet.range('AA:AA').autofit()
+        sheet.range('AB:AB').autofit()
+        sheet.range('AC:AC').autofit()
+        sheet.range('AD:AD').autofit()
+        sheet.range('AE:AE').autofit()
+        sheet.range('AF:AF').autofit()
+        sheet.range('AG:AG').autofit()
+        sheet.range('AH:AH').autofit()
+        sheet.range('AI:AI').autofit()
+        sheet.range('AJ:AJ').autofit()
+        sheet.range('AK:AK').autofit()
+        sheet.range('AL:AL').autofit()
+        sheet.range('AM:AM').autofit()
+        sheet.range('AN:AN').autofit()
 
 def hide_columns(sheet):
-    sheet.range('AI:AN').column_width = 0
-    sheet.range('AC:AF').column_width = 0
-    sheet.range('AB:AB').column_width = 10
-    sheet.range('S:AA').column_width = 0
-    sheet.range('Q:Q').column_width = 0
-    sheet.range('P:P').column_width = 20
-    sheet.range('O:O').column_width = 0
-    sheet.range('L:L').column_width = 0
-    # sheet.range('F:G').column_width = 0
-    # sheet.range('B:B').column_width = 0
+    skip_sheets = ['Config', 'Cover', 'Summary', 'Technical_Notes', 'T&C']
+    if sheet not in skip_sheets:
+        sheet.range('AI:AN').column_width = 0
+        sheet.range('AC:AF').column_width = 0
+        sheet.range('AB:AB').column_width = 10
+        sheet.range('S:AA').column_width = 0
+        sheet.range('Q:Q').column_width = 0
+        sheet.range('P:P').column_width = 20
+        sheet.range('O:O').column_width = 0
+        sheet.range('L:L').column_width = 0
+        # sheet.range('F:G').column_width = 0
+        # sheet.range('B:B').column_width = 0
 
 def summary(wb, discount=True):
     summary_formula = []
