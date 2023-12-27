d8ee1eb3404a528ee70649bba821610f623279e5
Thiha Aung <infowizard@gmail.com>
2023-12-28 06:05:55 +0800

    Create directory

--- checklists.py
+++ checklists.py
@@ -1,16 +1,48 @@
+""" 
+Creating checklists. This may later be turned into a class.
+
+"""
+
 from reportlab.pdfgen import canvas
-from reportlab.lib.colors import blue, green, yellow
+# from reportlab.lib.colors import blue, green, yellow
 import os
 import tempfile
 from pathlib import Path
 import subprocess
+import requests
 
-temp_directory = tempfile.TemporaryDirectory()
+# The checklist will be written to the download folder of the user.
+# Meant to be saved by the user at the desired location.
+downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+# temp_directory = tempfile.TemporaryDirectory()
 filename = 'checklist.pdf'
-c = canvas.Canvas(str(Path(temp_directory.name, filename)))
-c.drawString(10, 650, "Dog:")
+file_path = Path(downloads_folder, filename)
+
+# Download necessary files to local machine in 'Documents' folder
+try:
+    bid = os.path.join(os.path.expanduser('~/Documents'), 'Bid')
+    if not os.path.exists(bid):
+        os.makedirs(bid)
+except Exception as e:
+    pass
+
+
+
+c = canvas.Canvas(str(file_path))
+c.drawString(100, 650, "Dog:")
+c.drawString(100, 550, "Mee Mee:")
 c.showPage()
 c.save()
-# os.system(str(Path(temp_directory.name, filename)))
-# subprocess.Popen(Path(temp_directory.name, filename))
-# subprocess.call(Path(temp_directory.name, filename))
+
+# Open in system pdf application
+try:
+    if os.name == 'posix':
+        subprocess.call(['open', str(file_path)])
+    elif os.name == 'nt':
+        subprocess.call(['start', str(file_path)])
+except Exception as e:
+    print(f'Unsupported os {e}.')
+
+print(file_path)
+
